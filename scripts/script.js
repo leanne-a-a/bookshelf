2aaf59740106e788a726a3ddd0566b25c90d6eb4
leanne-a-a <150386565+leanne-a-a@users.noreply.github.com>
2023-11-21 13:23:09 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
+/*  
+Name: Leanne Afrifa 
+File Name: script.js
+*/
 
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
