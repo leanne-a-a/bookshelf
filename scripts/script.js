7741b36a3bbdcd0eb5105ea006a9092fdc19d0b5
leanne-a-a <150386565+leanne-a-a@users.noreply.github.com>
2023-11-21 13:36:46 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,19 @@ Name: Leanne Afrifa
 File Name: script.js
 */
 
-//Hamburger menu function
-function hamburger() {
-    var menu = document.getElementById("menu-links");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-        } else {
-            menu.style.display = "block";
-        }
-}
+
+//Function to provide answers to FAQs about books
+var faqItems = document.querySelectorAll(".faq");
+
+faqItems.forEach(function(item) {
+  var question = item.querySelector("h2");
+  var answer = item.querySelector(".answer");
+  
+  question.addEventListener("click", function() {
+    if (answer.style.display === "none") {
+      answer.style.display = "block";
+    } else {
+      answer.style.display = "none";
+    }
+  });
+});
